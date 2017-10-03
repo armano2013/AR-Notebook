65a89373f36e1ad9c063545be862bdaf6dac801e
Megan Majewski <meganmajewski@megans-mac-mini.attlocal.net>
2017-10-03 13:17:09 -0400

    add get from clipboard button

--- test_notebook/test_notebook/ViewController.swift
+++ test_notebook/test_notebook/ViewController.swift
@@ -24,23 +24,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func add(_ sender: Any) {
-        let node = SCNNode()
-        node.name = "node"
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.position = SCNVector3(0,0,-0.3)
-        self.sceneView.scene.rootNode.addChildNode(node)
-    }
-    @IBAction func reset_org(_ sender: Any) {
-        let node2 = SCNNode()
-        node2.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
-        node2.geometry?.firstMaterial?.diffuse.contents = UIColor.red
-        node2.position = SCNVector3(0,0,-0.3)
+    @IBAction func addFromClip(_ sender: Any) {
+        let ourText = SCNText(string: getClipboard(), extrusionDepth: 1.0)
+        let material = SCNMaterial()
+        
+        material.diffuse.contents = UIColor.red
+        
+        ourText.materials = [material]
+        
+        let node = SCNNode();
+        node.geometry = ourText;
+        
+        node.scale = SCNVector3(x: 0.01, y:0.01, z:0.01)
+        node.position = SCNVector3(0.01, 0.01, -0.01)
+        
+        sceneView.scene.rootNode.addChildNode(node)
 
-        if let testNode = self.sceneView.scene.rootNode.childNode(withName: "node", recursively: true) {
-                self.sceneView.scene.rootNode.replaceChildNode(testNode, with:node2)
+    }
+    func getClipboard() -> String{
+        let pasteboard: String? = UIPasteboard.general.string
+        if let string = pasteboard {
+            return "hi the string is: \(string)"
         }
+        return "else"
     }
 }
 
