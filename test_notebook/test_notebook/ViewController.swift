0acfef7a62bb335f23505824f3924a1017c51ad4
Megan Majewski <meganmajewski@megans-mac-mini.attlocal.net>
2017-10-03 20:20:17 -0400

    Add gallery picking function - post chosen image in AR

--- test_notebook/test_notebook/ViewController.swift
+++ test_notebook/test_notebook/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 import ARKit
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
     
     @IBOutlet weak var sceneView: ARSCNView!
     let configuration = ARWorldTrackingConfiguration()
+    let imagePicker = UIImagePickerController()
     override func viewDidLoad() {
         super.viewDidLoad()
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
@@ -31,10 +32,8 @@ class ViewController: UIViewController {
         material.diffuse.contents = UIColor.red
         
         ourText.materials = [material]
-        
         let node = SCNNode();
         node.geometry = ourText;
-        
         node.scale = SCNVector3(x: 0.01, y:0.01, z:0.01)
         node.position = SCNVector3(0.01, 0.01, -0.01)
         
@@ -48,5 +47,31 @@ class ViewController: UIViewController {
         }
         return "else"
     }
+    
+    //gallery
+    @IBAction func addImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        
+        imagePicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        imagePicker.allowsEditing = false
+        self.present(imagePicker, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            let node = SCNNode()
+            node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.001, chamferRadius: 0)
+            node.geometry?.firstMaterial?.diffuse.contents = UIImage.animatedImage(with: [pickedImage], duration: 0)
+            node.position = SCNVector3(0.1,0.1,0.1)
+            sceneView.scene.rootNode.addChildNode(node)
+        }
+        else{
+            //
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
