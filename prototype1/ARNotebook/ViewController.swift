95f2723ebc639ca8c3b1f4ba53627c65bf0ea7d3
Mohammed Ali <fh0420@wayne.edu>
2017-11-13 17:04:04 -0500

    page color fix and book style button dissmal upon selection

--- prototype1/ARNotebook/ViewController.swift
+++ prototype1/ARNotebook/ViewController.swift
@@ -86,7 +86,7 @@ class ViewController:  UIViewController, ARSCNViewDelegate, UIImagePickerControl
         self.registerGestureRecognizers()
         /// Create a session configuration
         self.registerGestureRecognizers()
-        //self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         // Run the view's session
         self.configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
